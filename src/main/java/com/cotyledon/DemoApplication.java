960c174bf8e82b049217be30be794c5c10f66d35
王慧倩 <ann01.wang@irootech.com>
2017-10-17 21:26:43 +0800

    添加eureka完成提交

--- src/main/java/com/cotyledon/DemoApplication.java
+++ src/main/java/com/cotyledon/DemoApplication.java
@@ -3,8 +3,12 @@ package com.cotyledon;
 import org.springframework.boot.SpringApplication;
 //如果使用SpringBootApplication需要加载此类
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@EnableEurekaClient使用
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
